5d3eaf24be24cb4a5f56a67158898ca1b1aa88f3
Tobias Locher <Tobi1879@gmail.com>
2023-10-30 08:15:18 +0100

    Added video.statistics and video.more intent

--- BE/index.py
+++ BE/index.py
@@ -15,8 +15,11 @@ youtube = build('youtube', 'v3', developerKey=api_key)
 # path to the key-file
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"]='./yt-chatbot-g99r-482b927c4e27.json'
 
+last_video_id = None
 
 def get_output(response):
+    global last_video_id
+    global last_video_id
     output = {
         "message": response['response']
     }
@@ -52,11 +55,65 @@ def get_output(response):
             ).execute()
 
             for search_result in search_response.get('items', []):
-                video_id = search_result['id']['videoId']
+                last_video_id = search_result['id']['videoId']
                 video_title = search_result['snippet']['title']
-                video_iframe = f'https://www.youtube.com/embed/{video_id}'
+                video_iframe = f'https://www.youtube.com/embed/{last_video_id}'
                 output['video_iframe'] = video_iframe
                 output['video_title'] = video_title
+    elif intent_name == 'video.statistics':
+        if last_video_id:
+            video_response = youtube.videos().list(
+                part='statistics,snippet',
+                id=last_video_id
+            ).execute()
+            video_statistics = video_response['items'][0]['statistics']
+            output['view_count'] = video_statistics.get('viewCount', '0')
+            output['like_count'] = video_statistics.get('likeCount', '0')
+            output['dislike_count'] = video_statistics.get('dislikeCount', '0')
+            output['comment_count'] = video_statistics.get('commentCount', '0')
+            output['video_description'] = video_response['items'][0]['snippet']['description']
+            output['video_title'] = video_response['items'][0]['snippet']['title']
+    elif intent_name == 'video.more':
+        amount = response['query_result'].parameters.get('amount')
+        if amount != '':
+            channel, keyword, length = None, None, None
+            output_contexts = response['query_result'].output_contexts
+            for context in output_contexts:
+                if str(context.parameters.get('channel')): # das funktioniert noch nicht, wenn man zuerst Video sucht, dann stats anzeigt und dann noch more videos. Dann liest es hier vermutlich den channel nicht aus. Weil dann 2 intents hier drin sind. If nützt anscheinend nichts. Oder er verliert den channel und so aber das macht kein sinn eigentlich.
+                    channel = context.parameters.get('channel')
+                    keyword = context.parameters.get('keyword')
+                    length = context.parameters.get('length')
+                    break
+
+            if channel != '' and keyword != '':
+                # Führe die Kanalsuche durch
+                search_response = youtube.search().list(
+                    q=channel,
+                    type='channel',
+                    part='snippet',
+                    maxResults=1  # Wir suchen nur nach dem ersten gefundenen Kanal
+                ).execute()
+
+                # Durchlaufe die Suchergebnisse und extrahiere den Kanal-ID
+                channel_id = None
+                if 'items' in search_response:
+                    channel_id = search_response['items'][0]['id']['channelId']
+
+                search_response = youtube.search().list(
+                    q=keyword,
+                    type='video',
+                    part='id,snippet',
+                    channelId=channel_id,
+                    maxResults=amount,  # Anzahl der Suchergebnisse, die du erhalten möchtest
+                    videoDuration=length if length else 'any'
+                ).execute()
+
+                output['videos'] = []
+                for search_result in search_response.get('items', []):
+                    last_video_id = search_result['id']['videoId']
+                    video_title = search_result['snippet']['title']
+                    video_url = f'https://www.youtube.com/watch?v={last_video_id}'
+                    output['videos'].append({'video_url': video_url, 'video_title': video_title})
 
     return output
 
