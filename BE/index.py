8a45adc50c1b71e484f4e3b8ebd095cf89860514
Tobias Locher <Tobi1879@gmail.com>
2023-11-02 08:12:59 +0100

    kleine Änderungen

--- BE/index.py
+++ BE/index.py
@@ -123,37 +123,24 @@ def sendMessage():
         message = data['message']
         session_id = "no-session-id"
 
-        # new session?
-        if 'newSession' in data and data['newSession'] == 'true':
-            session_id = str(uuid.uuid4())
+        response = get_dialogflow_response('yt-chatbot-g99r', session_id, message, 'en')
 
-        response = detect_intent_demo('yt-chatbot-g99r', session_id, message, 'en')
-
-        output = get_output(response)
-
-        return output
+        return get_output(response)
     else:
         return "Invalid POST request, 'message' field is missing."
 
 
-def detect_intent_demo(project_id, session_id, text, language_code):
-    from google.cloud import dialogflow
-
+def get_dialogflow_response(project_id, session_id, text, language_code):
     session_client = dialogflow.SessionsClient()
-
     session = session_client.session_path(project_id, session_id)
-
     text_input = dialogflow.TextInput(text=text, language_code=language_code)
-
     query_input = dialogflow.QueryInput(text=text_input)
 
     response = session_client.detect_intent(
         request={"session": session, "query_input": query_input}
     )
 
-    returnArray = {'query_result': response.query_result, 'response': format(response.query_result.fulfillment_text)}
-
-    return returnArray
+    return {'query_result': response.query_result, 'response': format(response.query_result.fulfillment_text)}
 
 
 if __name__ == '__main__':
